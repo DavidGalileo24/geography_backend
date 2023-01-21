9b543a542a5f11e4eeaa7371595435f22d794927
DavidGalileo24 <david.salgado.mejia@gmail.com>
2023-01-21 16:28:08 -0600

    Added migrations and files

---
+++ app/Http/Controllers/LanguajeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Languaje;
+use App\Http\Requests\StoreLanguajeRequest;
+use App\Http\Requests\UpdateLanguajeRequest;
+
+class LanguajeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLanguajeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLanguajeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Languaje  $languaje
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Languaje $languaje)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Languaje  $languaje
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Languaje $languaje)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateLanguajeRequest  $request
+     * @param  \App\Models\Languaje  $languaje
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateLanguajeRequest $request, Languaje $languaje)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Languaje  $languaje
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Languaje $languaje)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_01_21_221739_create_countries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('total_population', 11,2);
+            $table->string('capital');
+            $table->foreignId('continent_id')->constrained();
+            $table->foreignId('languaje_id')->constrained();
+            $table->foreignId('religion_id')->constrained();
+            $table->foreignId('currency_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+};
