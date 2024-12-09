02512190426f33c3774996ad72d9586405c280e8
DavidGalileo24 <david.salgado.mejia@dive.digital>
2024-12-09 12:03:56 -0600

    Showing data from index function

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Models\Continent;
+use App\Http\Controllers\ContinentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('/continents', ContinentController::class);
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('/continents', Continent::class);
+    
 });
 
